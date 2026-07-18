3847f6b88bcead2bf072bacbdbf9a1dec439eff1
M A Muqtadir <muqtxdir@gmail.com>
2026-07-18 16:09:59 +0530

    docs: expose them on gh pages (#1)
    * docs: add them `gi-docgen` descriptions
    
    * docs: add gi-docgen configuration and documentation files
    
    * ci: add the pages workflow
    
    * chore: add `README.md`

---
+++ doc/urlmap.js
@@ -0,0 +1,8 @@
+baseURLs = [
+    [ 'GLib', 'https://docs.gtk.org/glib/' ],
+    [ 'GObject', 'https://docs.gtk.org/gobject/' ],
+    [ 'Gio', 'https://docs.gtk.org/gio/' ],
+    [ 'Gdk', 'https://docs.gtk.org/gdk4/' ],
+    [ 'Gsk', 'https://docs.gtk.org/gsk4/' ],
+    [ 'Gtk', 'https://docs.gtk.org/gtk4/' ],
+]
